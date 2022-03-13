86f886461995b15480ee1e02e0ce0f11212fbac0
Pascal S. de Kloe <pascal@quies.net>
2022-03-13 17:19:25 +0100

    separate files for each ERC-721 interface

---
+++ contracts/ERC721TokenReceiver.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.4;
+
+/// @dev Note: the ERC-165 identifier for this interface is 0x150b7a02.
+interface ERC721TokenReceiver {
+	/// @notice Handle the receipt of an NFT
+	/// @dev The ERC721 smart contract calls this function on the recipient
+	///  after a `transfer`. This function MAY throw to revert and reject the
+	///  transfer. Return of other than the magic value MUST result in the
+	///  transaction being reverted.
+	///  Note: the contract address is always the message sender.
+	/// @param operator The address which called `safeTransferFrom` function
+	/// @param from The address which previously owned the token
+	/// @param tokenId The NFT identifier which is being transferred
+	/// @param data Additional data with no specified format
+	/// @return `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+	///  unless throwing
+	function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns(bytes4);
+}
