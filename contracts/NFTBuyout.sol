42b867b7e66298039d66a603f27c34b42ad6abb4
Pascal S. de Kloe <pascal@quies.net>
2023-05-23 18:58:59 +0200

    currency term—token ambiguous

--- contracts/NFTBuyout.sol
+++ contracts/NFTBuyout.sol
@@ -10,7 +10,7 @@ import "./ERC721TokenReceiver.sol";
 
 /// @title NFT Purchase En Masse
 /// @notice A buyout trades non-fungible tokens for fungible-token returns. Any
-///  number of buyers can make an offer to (the same) contract. It is up to the
+///  number of buyers can make an offer (to the same contract). It is up to the
 ///  individual NFT owners on whether to make use of any of such offers or not.
 /// @dev Only one contract required per chain. Look for NFTBuyoutOffer events to
 ///  find an existing one.
@@ -21,20 +21,20 @@ contract NFTBuyout {
 /// @param buyer The acquisition party
 event NFTBuyoutOffer(address indexed target, address buyer);
 
-/// @param RampDown Decrease the amount offered, per token identifier, with a
+/// @param RampDown Decrease the amount offered per token identifier, with a
 ///  fixed quantity, starting with zero, as in: price − (tokenID × varyAmount).
 enum TradeVary { RampDown }
 
 // Each NFT-contract:buyer:conditions entry is an individual buyout attempt.
 mapping(address => mapping(address => Conditions)) private buyouts;
 
-/// @param token The ERC-20 unit
+/// @param currency The ERC-20 unit
 /// @param decimals The ERC-20 quantity exponent
 /// @param price The token quantity
 /// @param varyType The price variant
 /// @param varyAmount The varyType quantity
 struct Conditions {
-	address   token;
+	address   currency;
 	uint8     decimals;
 	uint40    price;
 	TradeVary varyType;
@@ -52,24 +52,24 @@ function offer(address target, Conditions calldata c) public payable {
 	}
 
 	// NFT contracts MUST implement ERC-165 by spec
-	require(ERC165(target).supportsInterface(type(ERC721).interfaceId), "need standard NTF");
+	require(ERC165(target).supportsInterface(type(ERC721).interfaceId), "need standard NFT");
 
 	// fail-fast: trade requires allowance to this contract
-	require(ERC20(c.token).allowance(msg.sender, address(this)) != 0, "no payment allowance");
+	require(ERC20(c.currency).allowance(msg.sender, address(this)) != 0, "no payment allowance");
 
 	buyouts[target][msg.sender] = c;
 	emit NFTBuyoutOffer(target, msg.sender);
 }
 
-/// @notice Each token is subject to a dedicated trade amount.
+/// @notice Each NFT is subject to a dedicated trade amount.
 /// @dev ⚠️ Newly minted tokens may alter expectations.
 /// @param target The ERC-721 contract
 /// @param tokenID The NFT in subject
 /// @return amount The ERC-20 quantity
-/// @return token The ERC-20 contract
-function tokenPrice(address target, uint256 tokenID, address buyer) public view returns (uint256 amount, address token) {
+/// @return currency The ERC-20 contract
+function tokenPrice(address target, uint256 tokenID, address buyer) public view returns (uint256 amount, address currency) {
 	Conditions memory c = buyouts[target][buyer];
-	return (tokenPriceFromConditions(c, tokenID), c.token);
+	return (tokenPriceFromConditions(c, tokenID), c.currency);
 }
 
 function tokenPriceFromConditions(Conditions memory c, uint256 tokenID) private pure returns (uint256 amount) {
@@ -84,20 +84,20 @@ function tokenPriceFromConditions(Conditions memory c, uint256 tokenID) private 
 /// @notice Trade an NFT for ERC-20.
 /// @dev Offers can be modified or retracted.
 /// @param target The ERC-721 contract
-/// @param tokenID The NTF in subject
+/// @param tokenID The NFT in subject
 /// @param buyer The acquisition party
-/// @param wantAmount The minimal price expectation (to prevent races)
-/// @param wantToken The ERC-20 unit expectation (to prevent races)
-function redeemToken(address target, uint256 tokenID, address buyer, uint256 wantAmount, address wantToken) public {
+/// @param wantPrice The minimal amount expectation—prevents races
+/// @param wantCurrency The ERC-20 unit expectation—prevents races
+function redeemToken(address target, uint256 tokenID, address buyer, uint256 wantPrice, address wantCurrency) public {
 	require(msg.sender != buyer, "sell to self");
 
 	Conditions memory c = buyouts[target][buyer];
-	uint256 amount = tokenPriceFromConditions(c, tokenID);
-	require(amount >= wantAmount, "trade amount mismatch");
-	require(c.token == wantToken, "trade token mismatch");
+	uint256 price = tokenPriceFromConditions(c, tokenID);
+	require(price >= wantPrice, "trade price miss");
+	require(c.currency == wantCurrency, "trade currency miss");
 
 	ERC721(target).transferFrom(msg.sender, buyer, tokenID);
-	ERC20(c.token).transferFrom(buyer, msg.sender, amount);
+	ERC20(wantCurrency).transferFrom(buyer, msg.sender, price);
 }
 
 }
